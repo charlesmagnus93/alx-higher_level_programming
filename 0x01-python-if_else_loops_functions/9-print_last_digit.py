2cf618e1f742d6167f5b44720ecb0a290e2161f3
charlemagne <agbolossouc@gmail.com>
2022-06-28 13:55:14 -0700

    9

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    print(str(number)[-1:], end='')
-    return (str(number)[-1:])
+    if (number.isnumeric()):
+        print(str(number)[-1:], end='')
+        return (str(number)[-1:])
