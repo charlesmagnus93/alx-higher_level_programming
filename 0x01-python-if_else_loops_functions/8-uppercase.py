6aabb0bc49f09dfdda884f6c69179cb545050749
charlemagne <agbolossouc@gmail.com>
2022-06-28 13:07:21 -0700

    8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def uppercase(str):
+    a = ''
+    for i in str:
+        if (i.isnumeric()):
+            a+= i
+            continue
+        if (ord(i) >= 65 and ord(i)<= 91):
+            a+= i
+            continue
+        b = ord(i) - 32
+        a+= f'{chr(b)}'
+    
+    print(a)
