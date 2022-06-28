c211fddf81a79ba7fa4f0536c029079fa3c5ebe0
charlemagne <agbolossouc@gmail.com>
2022-06-28 13:43:07 -0700

    12

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    a = ''
+    for i in range(1, 101):
+        if (i % 3 == 0 and i % 5 != 0 ):
+            a += f'Frizz '
+        elif (i % 5 == 0 and i % 3 != 0):
+            a += f'Buzz '
+        elif (i % 3 == 0 and i % 5 == 0):
+            a += f'FizzBuzz '
+        else:
+            a += f'{str(i)} '
+    print(a)
