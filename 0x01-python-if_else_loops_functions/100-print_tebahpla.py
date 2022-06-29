0db9245dc24b3bfd30c534f503e67db73377f12f
charlemagne <agbolossouc@gmail.com>
2022-06-29 09:36:37 -0700

    100

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 for a in range(97, 123)[::-1]:
-    print(('{}'.format(chr(a)) if a % 2 == 0 else '{}'.format(chr(a - 32))), end='')
+    print(
+        ('{}'.format(chr(a)) if a % 2 == 0 else '{}'.format(chr(a - 32))),
+        end=''
+    )
