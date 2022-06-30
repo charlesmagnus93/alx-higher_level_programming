6139e7c60449f46c916017c4aa541541e65c8f74
charlemagne <agbolossouc@gmail.com>
2022-06-30 13:22:02 -0700

    100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -12,13 +12,25 @@ else:
         exit(1)
     else:
         if argv[2] == '+':
-            print('{} + {} = {}'.format(argv[1], argv[3], add(int(argv[1]), int(argv[3]))))
+            print(
+                '{} + {} = {}'
+                .format(argv[1], argv[3], add(int(argv[1]), int(argv[3])))
+            )
         elif argv[2] == '-':
-            print('{} - {} = {}'.format(argv[1], argv[3], sub(int(argv[1]), int(argv[3]))))
+            print(
+                '{} - {} = {}'
+                .format(argv[1], argv[3], sub(int(argv[1]), int(argv[3])))
+            )
         elif argv[2] == '*':
-            print('{} * {} = {}'.format(argv[1], argv[3], mul(int(argv[1]), int(argv[3]))))
+            print(
+                '{} * {} = {}'
+                .format(argv[1], argv[3], mul(int(argv[1]), int(argv[3])))
+            )
         elif argv[2] == '/':
-            print('{} / {} = {}'.format(argv[1], argv[3], div(int(argv[1]), int(argv[3]))))
+            print(
+                '{} / {} = {}'
+                .format(argv[1], argv[3], div(int(argv[1]), int(argv[3])))
+            )
 
 
 if __name__ == "__main__":
