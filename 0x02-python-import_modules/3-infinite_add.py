d9fbd869e7ba80b2132e045ab95e4b24ed72fa68
charlemagne <agbolossouc@gmail.com>
2022-06-30 12:31:53 -0700

    3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 from sys import argv
 a = 0
-for i in list(argv[1]):
-    a += int(i)
+for i, arg in enumerate(argv, start=0):
+    if i == 0:
+        continue
+    a += int(argv[i])
 print('{}'.format(a))
 
 if __name__ == "__main__":
