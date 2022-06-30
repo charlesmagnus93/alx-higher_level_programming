b46e8fb5393a1c1a60bfcfcc8c0c4faa0b8e3452
charlemagne <agbolossouc@gmail.com>
2022-06-30 12:29:14 -0700

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,10 +3,10 @@ from sys import argv
 if len(argv) == 1:
     print('{} arguments.'.format(len(argv) - 1))
 elif len(argv) == 2:
-    print('{} argument.'.format(len(argv) - 1))
+    print('{} argument:'.format(len(argv) - 1))
     print('{}: {}'.format(len(argv) - 1, argv[1]))
 else:
-    print('{} arguments.'.format(len(argv) - 1))
+    print('{} arguments:'.format(len(argv) - 1))
     for i, arg in enumerate(argv, start=0):
         if i == 0:
             continue
