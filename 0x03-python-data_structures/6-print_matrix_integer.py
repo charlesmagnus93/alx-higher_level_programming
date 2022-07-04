8cba1984c542fd5b695323ed292d16229de14615
charlemagne <agbolossouc@gmail.com>
2022-07-04 08:06:29 -0700

    6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,6 @@ def print_matrix_integer(matrix=[[]]):
     a = ''
     for x in matrix:
         for y in x:
-            a += '{} '.format(y)
+            a += '{:d} '.format(y)
         print('{}'.format(a).rstrip())
         a = ''
