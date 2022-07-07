f7e4e768602644eaa7a130b9f9d33f11ddcdb993
charlemagne <agbolossouc@gmail.com>
2022-07-07 06:44:35 -0700

    10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+    best = 0
+    name = ''
+    if a_dictionary == None:
+        return None
+    else:
+        keys = sorted(a_dictionary)
+        for x in keys:
+            if a_dictionary[x] > best:
+                best = a_dictionary[x]
+                name = x
+    return name
