6a781bf6093a2d16cb15cfc614f2ebc4be243cb9
charlemagne <agbolossouc@gmail.com>
2022-07-07 06:50:18 -0700

    10-best

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -8,7 +8,10 @@ def best_score(a_dictionary):
     else:
         keys = sorted(a_dictionary)
         for x in keys:
-            if a_dictionary[x] > best:
-                best = a_dictionary[x]
-                name = x
+            if a_dictionary[x] is not None:
+                if a_dictionary[x]   > best:
+                    best = a_dictionary[x]
+                    name = x
+            else:
+                continue  
     return name
