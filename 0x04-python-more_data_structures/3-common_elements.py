8421b6486d04c1ebe4e1d55029bff001259134b1
charlemagne <agbolossouc@gmail.com>
2022-07-07 05:15:21 -0700

    3

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def common_elements(set_1, set_2):
+    el= []
+    for x in set_1:
+        if (x in set_2):
+            el.append(x)
+        else:
+            continue
+    return el
