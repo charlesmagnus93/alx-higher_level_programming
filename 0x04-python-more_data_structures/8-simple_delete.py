e17bc7532ca661e414a7c2fdb036847a7dfc2614
charlemagne <agbolossouc@gmail.com>
2022-07-07 06:24:54 -0700

    8

---
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def simple_delete(a_dictionary, key=""):
+    sort = sorted(a_dictionary)
+    for x in sort:
+        if x == key:
+            a_dictionary.pop(key)
+            
+    return a_dictionary
