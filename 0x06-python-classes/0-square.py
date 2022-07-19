9c077941a1ea308cc9a28c0346e5e28846d0ec49
charlemagne <agbolossouc@gmail.com>
2022-07-19 13:30:16 -0700

    1

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-
 class Square:
     """Square definition"""
 
-    def __init__(self):
+    def __init__(self) -> None:
         """Initialization"""
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+class Square:
+    """Square"""
+
+    def __init__(self, size) -> None:
+        """Initialization"""
+        self.__size = size
