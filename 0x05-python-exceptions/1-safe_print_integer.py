eb3de7a3eb8d0f31aeee3122d7543ba76df2e234
charlemagne <agbolossouc@gmail.com>
2022-07-20 05:51:48 -0700

    1-s

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -6,6 +6,6 @@ def safe_print_integer(value):
             print("{:d}".format(value))
             return True
         else:
-            return False
+            return Exception
     except:
         return False
