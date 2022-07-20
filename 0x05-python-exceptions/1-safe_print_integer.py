9c6f2d1d311c47f197baa0c8998757422e902ce2
charlemagne <agbolossouc@gmail.com>
2022-07-20 05:51:04 -0700

    1-s

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,7 +2,7 @@
 
 def safe_print_integer(value):
     try:
-        if any(type(el) == int for el in value):
+        if any(el.isdigit() for el in value):
             print("{:d}".format(value))
             return True
         else:
