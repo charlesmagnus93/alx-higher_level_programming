32ad85efffe303904a84f1f81d5695ed093a5c29
charlemagne <agbolossouc@gmail.com>
2022-07-20 04:05:40 -0700

    5-

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+"""
+no module
+"""
+
+
+class Square:
+    """
+    Square definition
+    def __init__(self, size=0):
+    """
+    def __init__(self, size=0):
+        """
+        Initialization
+        --------
+        size: interger
+        """
+        self.__size = size
+        try:
+            assert (isinstance(size, int))
+        except Exception:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+
+    @property
+    def size(self):
+        """
+        retrieve private attri
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        set private sttri
+        """
+        self.__size = value
+        try:
+            assert (isinstance(value, int))
+        except Exception:
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+
+    def area(self):
+        """
+        Area of Square
+        """
+        return self.__size ** 2
+
+    def my_print(self):
+        """
+        print square with caracter #
+        """
+        if self.__size == 0:
+            print()
+        for x in range(self.__size):
+            for y in range(self.__size):
+                print('#', end="")
+            print()
