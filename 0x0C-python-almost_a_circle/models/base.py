c4d58be5b3e3ef91c67f2db75fe41ce52ebdc16c
charlemagne <agbolossouc@gmail.com>
2022-08-12 09:43:53 -0700

    1

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -14,7 +14,7 @@ class Base:
         """
         Initialization
         """
-        if id != None:
+        if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
