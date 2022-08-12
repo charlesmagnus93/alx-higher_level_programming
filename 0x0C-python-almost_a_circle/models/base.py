829c7ec4f28956719ba436c2960783715cf48f65
charlemagne <agbolossouc@gmail.com>
2022-08-12 09:26:46 -0700

    1

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+no module
+"""
+
+
+class Base:
+    """
+    Base definition
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+        Initialization
+        """
+        if id == None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
