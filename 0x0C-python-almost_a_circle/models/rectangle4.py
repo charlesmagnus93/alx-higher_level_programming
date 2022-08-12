aae0c084a0d7128d3a9c5dd49707788b2a477240
charlemagne <agbolossouc@gmail.com>
2022-08-12 10:28:38 -0700

    5

--- 0x0C-python-almost_a_circle/models/rectangle4.py
+++ 0x0C-python-almost_a_circle/models/rectangle4.py
@@ -94,11 +94,3 @@ class Rectangle(Base):
             for j in range(self.__width):
                 print("#", end="")
             print()
-
-    def __str__(self):
-        """str method to return rectangle representation"""
-        return '[Rectangle] ({}) {}/{} - {}/{}'.format(self.id,
-            self.__x,
-            self.__y,
-            self.__width,
-            self.__height)
