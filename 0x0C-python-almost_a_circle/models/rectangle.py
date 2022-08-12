68a1f76b82700047daa5f39e631494b231320511
charlemagne <agbolossouc@gmail.com>
2022-08-12 10:37:41 -0700

    7

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -108,3 +108,18 @@ class Rectangle(Base):
             self.__width,
             self.__height
         )
+
+    def update(self, *args):
+        """assigns an argument to each list"""
+        if (args):
+            for i, j in enumerate(args):
+                if i == 0:
+                    self.id = j
+                elif i == 1:
+                    self.width = j
+                elif i == 2:
+                    self.height = j
+                elif i == 3:
+                    self.x = j
+                elif i == 4:
+                    self.y = j
