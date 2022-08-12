65ae43f98e76cb121bf17285c74a2137167ba57b
charlemagne <agbolossouc@gmail.com>
2022-08-12 10:15:25 -0700

    2

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -19,3 +19,43 @@ class Rectangle(Base):
         self.height = height
         self.x = x
         self.y = y
+
+    @property
+    def width(self):
+        """retrieve"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """setter"""
+        self.__width = value
+
+    @property
+    def height(self):
+        """retrieve"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """setter"""
+        self.__height = value
+
+    @property
+    def x(self):
+        """retrieve"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """setter"""
+        self.__x = value
+
+    @property
+    def y(self):
+        """retrieve"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """setter"""
+        self.__y = value
