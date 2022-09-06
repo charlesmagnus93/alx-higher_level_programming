d352d1c313aa27db3d4606108c03e797b86544ab
charlemagne <agbolossouc@gmail.com>
2022-09-06 13:36:35 -0700

    group by

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- list number of score with the same score
-SELECT score, number FROM second_table GROUP BY score as number ORDER BY score DESC;
+SELECT score, COUNT(id) as number FROM second_table GROUP BY score ORDER BY number DESC;
