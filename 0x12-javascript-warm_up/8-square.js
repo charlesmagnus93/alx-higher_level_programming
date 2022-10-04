8728b4be2e1e620721c900cc7911c45a14a6474b
charlemagne <agbolossouc@gmail.com>
2022-10-04 03:59:47 -0700

    square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const arg = process.argv;
+
+if ((arg[2] && Number.isInteger(parseInt(arg[2])))) {
+  let str = '';
+  for (let i = 0; i < parseInt(arg[2]); i++) {
+    for (let y = 0; y < parseInt(arg[2]); y++) {
+      str += 'X';
+    }
+    str += '\n';
+  }
+  console.log(str);
+} else {
+  console.log('Missing size');
+}
