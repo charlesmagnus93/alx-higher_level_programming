b90c335b8c2968909a1d2c754187bd6201dddc69
charlemagne <agbolossouc@gmail.com>
2022-10-05 12:25:38 -0700

    square update

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,16 +1,21 @@
 #!/usr/bin/node
 
 const arg = process.argv;
-
-if ((arg[2] && Number.isInteger(parseInt(arg[2])))) {
-  let str = '';
-  for (let i = 0; i < parseInt(arg[2]); i++) {
-    for (let y = 0; y < parseInt(arg[2]); y++) {
-      str += 'X';
+if (arg[2] > 0) {
+  if ((arg[2] && Number.isInteger(parseInt(arg[2])))) {
+    let str = '';
+    for (let i = 0; i < parseInt(arg[2]); i++) {
+      for (let y = 0; y < parseInt(arg[2]); y++) {
+        str += 'X';
+      }
+      if (i === (parseInt(arg[2]) - 1)) {
+        str += '';
+      } else {
+        str += '\n';
+      }
     }
-    str += '\n';
+    console.log(str);
+  } else {
+    console.log('Missing size');
   }
-  console.log(str);
-} else {
-  console.log('Missing size');
 }
