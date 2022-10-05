a7d1f5df4256f32d9a8979e7ded889b6635f102c
charlemagne <agbolossouc@gmail.com>
2022-10-05 14:13:44 -0700

    concat files

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -5,4 +5,4 @@ const { readFileSync, writeFileSync } = require('fs');
 const content1 = readFileSync('fileA', 'utf-8');
 const content2 = readFileSync('fileB', 'utf-8');
 writeFileSync('fileC', content1 + '\n' + content2, 'utf-8');
-console.log(readFileSync('fileC', 'utf-8'));
+console.log(content1 + content2);
