7b78abf88cda3d6931ae1de067f16a9ddf1f3907
charlemagne <agbolossouc@gmail.com>
2022-10-08 10:07:23 -0700

    pycodestyle

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,17 +7,17 @@ Request to DB using argements
 """
 
 conn = MySQLdb.connect(
-  host="localhost",
-  port=3306,
-  user=argv[1],
-  passwd=argv[2],
-  db=argv[3],
-  charset="utf8"
+    host="localhost",
+    port=3306,
+    user=argv[1],
+    passwd=argv[2],
+    db=argv[3],
+    charset="utf8"
 )
 cur = conn.cursor()
 cur.execute("SELECT * FROM states ORDER BY id ASC")
 query_rows = cur.fetchall()
 for row in query_rows:
-  print(row)
+    print(row)
 cur.close()
 conn.close()
