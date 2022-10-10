80a9855cfae857a8ebecacb49b242cdadd7e74fd
charlemagne <agbolossouc@gmail.com>
2022-10-10 11:35:53 -0700

    bugs

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,21 +6,27 @@ import MySQLdb
 Request to DB using argements
 """
 
-conn = MySQLdb.connect(
-    host="localhost",
-    port=3306,
-    user=argv[1],
-    passwd=argv[2],
-    db=argv[3],
-    charset="utf8"
-)
-cur = conn.cursor()
-cur.execute("SELECT * FROM states ORDER BY id ASC")
-query_rows = cur.fetchall()
-for row in query_rows:
-    print(row)
-cur.close()
-conn.close()
+
+def main():
+    """
+    Create connexion to sql DB and make request
+    """
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+        charset="utf8"
+    )
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
+
 
 if __name__ == "__main__":
-    argv, MySQLdb
+    main()
---
+++ 0x0F-python-object_relational_mapping/a.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+
+print(__import__("0-select_states").__doc__)
