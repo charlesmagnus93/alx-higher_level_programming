e070cba55f64f6ae6f07b5f4c79421c104b3333d
charlemagne <agbolossouc@gmail.com>
2022-10-21 12:17:20 -0700

    fetch with urllib

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,6 +6,6 @@ with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
     html = response.read()
 
 print(
-    'Body response: \n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}'
+    'Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}'
     .format(type(html), html, html.decode())
 )
