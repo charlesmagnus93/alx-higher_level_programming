0d57b24b9505806be6ce5b29d9e7f206dca2743d
charlemagne <agbolossouc@gmail.com>
2022-10-21 12:15:11 -0700

    fetch with urllib

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+import urllib.request
+
+with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    html = response.read()
+
+print(
+    'Body response: \n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}'
+    .format(type(html), html, html.decode())
+)
