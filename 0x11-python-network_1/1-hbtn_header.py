612263f1727bd68f790140a2a4bb12c9562b1bb2
charlemagne <agbolossouc@gmail.com>
2022-10-21 13:05:57 -0700

    get response header using get

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
 
-import sys
-import urllib.request
-
-
-def main():
-    with urllib.request.urlopen(sys.argv[1]) as response:
-        print(response.headers.get('X-Request-Id'))
-
-
 if __name__ == '__main__':
-    main()
+    import sys
+    import urllib.request
+
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        print(response.info().get('X-Request-Id'))
