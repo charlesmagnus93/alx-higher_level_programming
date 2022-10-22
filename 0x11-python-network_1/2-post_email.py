54d78207b31cbf46cf1dd34c876dcb8cfc33216f
charlemagne <agbolossouc@gmail.com>
2022-10-21 17:38:31 -0700

    POST request with urllib

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
-if __name__ == '__main__':
-    import sys
-    import urllib.request
+import sys
+import urllib.request
 
-    data = urllib.parse.urlencode({"email": sys.argv[2]})
-    data = data.encode('ascii')
-    req = urllib.request.Request(sys.argv[1], data)
-    with urllib.request.urlopen(req) as response:
-        print(response.read().decode('utf-8'))
+data = urllib.parse.urlencode({"email": sys.argv[2]})
+data = data.encode('ascii')
+req = urllib.request.Request(sys.argv[1], data)
+with urllib.request.urlopen(req) as response:
+    print(response.read().decode('utf-8'))
+if __name__ == '__main__':
+    sys, urllib
