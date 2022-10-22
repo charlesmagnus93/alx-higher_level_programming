a65e5d71aef03a0158805820105756b0804a1ddb
charlemagne <agbolossouc@gmail.com>
2022-10-21 17:35:46 -0700

    requests mdule

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-# request module
-if __name__ == '__main__':
+# requests module
 
-    import requests
+import requests
 
-    req = requests.get('https://alx-intranet.hbtn.io/status')
+req = requests.get('https://alx-intranet.hbtn.io/status')
 
-    print(
-        'Body response:\n\t- type: {}\n\t- content: {}'
-        .format(type(req.text), req.text)
-    )
+print(
+    'Body response:\n\t- type: {}\n\t- content: {}'
+    .format(type(req.text), req.text)
+)
+
+if __name__ == "__main__":
+    requests
