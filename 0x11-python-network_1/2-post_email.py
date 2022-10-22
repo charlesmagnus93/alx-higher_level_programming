74bbc95fd5f2c18e39f35920da81f1e7a71ef116
charlemagne <agbolossouc@gmail.com>
2022-10-21 17:39:28 -0700

    POST request with urllib

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -9,4 +9,4 @@ req = urllib.request.Request(sys.argv[1], data)
 with urllib.request.urlopen(req) as response:
     print(response.read().decode('utf-8'))
 if __name__ == '__main__':
-    sys, urllib
+    sys, urllib.request
