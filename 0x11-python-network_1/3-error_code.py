e1df368a4630afd2725a3a7d904a892df452537c
charlemagne <agbolossouc@gmail.com>
2022-10-21 17:42:13 -0700

    Error handle

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 
-if __name__ == '__main__':
-    import sys
-    import urllib.request
+import sys
+import urllib.request
 
-    req = urllib.request.Request(sys.argv[1])
-    try:
-        with urllib.request.urlopen(req) as response:
-            print(response.read().decode('utf-8'))
-    except urllib.error.HTTPError as e:
-        print("Error code: {}".format(e.code))
+req = urllib.request.Request(sys.argv[1])
+try:
+    with urllib.request.urlopen(req) as response:
+        print(response.read().decode('utf-8'))
+except urllib.error.HTTPError as e:
+    print("Error code: {}".format(e.code))
+
+if __name__ == '__main__':
+    sys, urllib.request
