dfeea319adbd047293583c5f6e338614212ff0ab
charlemagne <agbolossouc@gmail.com>
2022-10-25 13:03:35 -0700

    print error

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-
-import sys
-import urllib.request
-
-req = urllib.request.Request(sys.argv[1])
-try:
-    with urllib.request.urlopen(req) as response:
-        print(response.read().decode('utf-8'))
-except urllib.error.HTTPError as e:
-    print("Error code: {}".format(e.code))
-
+"""
+print error code
+"""
 if __name__ == '__main__':
-    sys, urllib.request
+
+    import sys
+    import urllib.request
+
+    req = urllib.request.Request(sys.argv[1])
+    try: 
+        with urllib.request.urlopen(req) as response:
+            print(response.read().decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
