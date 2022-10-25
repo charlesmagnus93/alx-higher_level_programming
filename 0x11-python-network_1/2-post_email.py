66a703f8b4899948ba54122ebca17cc372af9148
charlemagne <agbolossouc@gmail.com>
2022-10-25 12:52:43 -0700

    post request

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-
-import sys
-import urllib.request
-
-data = urllib.parse.urlencode({"email": sys.argv[2]})
-data = data.encode('ascii')
-req = urllib.request.Request(sys.argv[1], data)
-with urllib.request.urlopen(req) as response:
-    print(response.read().decode('utf-8'))
+"""
+post request
+"""
 if __name__ == '__main__':
-    sys, urllib.request
+
+    import sys
+    import urllib.request
+
+    data = urllib.parse.urlencode({"email": sys.argv[2]})
+    data = data.encode('ascii')
+    req = urllib.request.Request(sys.argv[1], data)
+    with urllib.request.urlopen(req) as response:
+        print(response.read().decode('utf-8'))
