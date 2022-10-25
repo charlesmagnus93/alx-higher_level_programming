7f83472c80c5e05bb04d0ff91a662e713a068104
charlemagne <agbolossouc@gmail.com>
2022-10-25 12:32:28 -0700

    control

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,10 +2,10 @@
 """
 get header
 """
-import sys
-import urllib.request
-
-with urllib.request.urlopen(sys.argv[1]) as response:
-    print(response.headers.get('X-Request-Id'))
 if __name__ == '__main__':
-    sys, urllib
+
+    import sys
+    import urllib.request
+
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        print(response.headers.get('X-Request-Id'))
