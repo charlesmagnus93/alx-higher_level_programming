f1f79ebf0cd090af490310cf3b71290a9fecbb4a
charlemagne <agbolossouc@gmail.com>
2022-10-25 17:17:21 -0700

    POST params

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     import requests
 
     url = "http://0.0.0.0:5000/search_user"
-    if sys.argv[1]:
+    if len(sys.argv[1]) > 1:
         q = sys.argv[1]
     else:
         q = ""
