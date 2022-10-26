a5a8d1a14053f16426a13dc2807b3ab8e5658fad
charlemagne <agbolossouc@gmail.com>
2022-10-25 17:49:58 -0700

    get me github id

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -6,5 +6,8 @@ if __name__ == "__main__":
 
     import sys
     import requests
-    req = requests.get("https://api.github.com/user", auth=(sys.argv[1], sys.argv[2]))
+    req = requests.get(
+            "https://api.github.com/user",
+            auth=(sys.argv[1], sys.argv[2])
+        )
     print(req.json().get('id'))
