9669b635c739d19f36452894d57b4d35520e5bcd
charlemagne <agbolossouc@gmail.com>
2022-10-25 17:48:03 -0700

    get me github id

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+request github api
+"""
+if __name__ == "__main__":
+
+    import sys
+    import requests
+    req = requests.get("https://api.github.com/user", auth=(sys.argv[1], sys.argv[2]))
+    print(req.json().get('id'))
