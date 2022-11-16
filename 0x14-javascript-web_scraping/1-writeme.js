f51b84399acf8689c8df9309a5c5cb8eb8372299
ch@rlemm@gne <agbolossouc@gmail.com>
2022-11-16 09:05:04 +0000

    write

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-const filePath = process.argv.slice(2)[0]
+const filePath = process.argv.slice(2)[0];
 const text = process.argv.slice(2)[1];
 
-fs.writeFile(filePath, text, {flag: 'w+'}, () => {});
+fs.writeFile(filePath, text, { flag: 'w+' }, () => {});
