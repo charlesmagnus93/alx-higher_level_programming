4c74a621222bc915b9f7f9dcdbdc276a7ad05758
ch@rlemm@gne <agbolossouc@gmail.com>
2022-11-16 09:42:13 +0000

    count

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+request('https://swapi-api.hbtn.io/api/films/', (err, response, body) => {
+  if (err) {
+    console.log(err);
+  }
+  const data = JSON.parse(body).results;
+
+  const count = data.reduce((count, item) => {
+    for (const url of item.characters) {
+      if (url.indexOf('18') !== -1) {
+        return count + 1;
+      }
+    }
+    return count;
+  }, 0);
+
+  console.log(count);
+});
