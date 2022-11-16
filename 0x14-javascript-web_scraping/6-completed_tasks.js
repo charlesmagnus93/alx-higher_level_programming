d6edcc96f69e8bcdbe7f3c79bdc1a36c3951ff67
ch@rlemm@gne <agbolossouc@gmail.com>
2022-11-16 09:56:17 +0000

    count

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = process.argv.slice(2)[0];
+
+request(url, (err, response, body) => {
+  if (err) {
+    console.log(err);
+  }
+
+  const todos = JSON.parse(body);
+
+  const users = {};
+  for (const todo of todos) {
+    if (users[todo.userId] === undefined) {
+      users[todo.userId] = 0;
+    }
+
+    if (todo.completed) {
+      users[todo.userId] += 1;
+    }
+  }
+  console.log(users);
+});
